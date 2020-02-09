5a4d320a7372b19ab97d37f041aba5c87f0d65b3
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-02-09 18:23:13 +0900

    Refine syntax and support non-usize keys, lower bounds of keys and expression as key

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //! ```
 //! use memoise::memoise;
 //!
-//! #[memoise(keys(n = 100))]
+//! #[memoise(n <= 100)]
 //! fn fib(n: usize) -> usize {
 //!     if n == 0 || n == 1 {
 //!         return n;
@@ -27,7 +27,7 @@
 //! ```
 //! use memoise::memoise;
 //!
-//! #[memoise(keys(n = 100, m = 50))]
+//! #[memoise(n <= 100, m <= 50)]
 //! fn comb(n: usize, m: usize) -> usize {
 //!     if m == 0 {
 //!         return 1;
@@ -58,19 +58,331 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use std::collections::HashMap;
-use syn::{parse_macro_input, parse_quote, AttributeArgs, Expr, Ident, ItemFn, ReturnType, Type};
+use std::ops::Deref;
+use syn::{
+    parse::{Parse, ParseStream, Result},
+    parse_macro_input, parse_quote, parse_str, AttributeArgs, BinOp, Error, Expr, ExprBinary,
+    ExprLit, ExprUnary, Ident, ItemFn, Lit, LitInt, ReturnType, Token, Type,
+};
 
-#[derive(Debug, FromMeta)]
-struct MemoiseArgs {
-    keys: HashMap<String, usize>,
+#[derive(PartialEq, Eq, Debug)]
+struct Key {
+    expr: Expr,
+    lower_bound: Option<(Expr, bool)>,
+    upper_bound: Option<(Expr, bool)>,
+}
+
+impl Key {
+    fn len(&self) -> Option<usize> {
+        assert!(!(self.lower_bound.is_some() && self.upper_bound.is_none()));
+
+        if let Some(ub) = &self.upper_bound {
+            let lb = self.offset();
+
+            let ub = (expr_to_i64(&ub.0), ub.1);
+            let ub = ub.0 + if ub.1 { 1 } else { 0 };
+
+            // range is [lb, ub)
+            Some((ub - lb) as usize)
+        } else {
+            None
+        }
+    }
+
+    fn offset(&self) -> i64 {
+        let lb = self
+            .lower_bound
+            .as_ref()
+            .map_or((0, true), |r| (expr_to_i64(&r.0), r.1));
+
+        lb.0 + if lb.1 { 0 } else { 1 }
+    }
+}
+
+// input must be `LitInt` or `- LitInt`
+fn expr_to_i64(e: &Expr) -> i64 {
+    match e {
+        Expr::Unary(ExprUnary { expr, .. }) => -expr_to_i64(expr.deref()),
+        Expr::Lit(ExprLit {
+            lit: Lit::Int(n), ..
+        }) => n.base10_parse().unwrap(),
+        _ => unreachable!(),
+    }
+}
+
+struct LitSignedInt(Expr);
+
+impl Parse for LitSignedInt {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let neg = if input.peek(Token![-]) {
+            Some(input.parse::<Token![-]>()?)
+        } else {
+            None
+        };
+        let lit = input.parse::<LitInt>()?;
+
+        let e = ExprLit {
+            attrs: vec![],
+            lit: lit.into(),
+        }
+        .into();
+
+        Ok(LitSignedInt(if let Some(neg) = neg {
+            ExprUnary {
+                attrs: vec![],
+                op: syn::UnOp::Neg(neg),
+                expr: Box::new(e),
+            }
+            .into()
+        } else {
+            e
+        }))
+    }
+}
+
+#[test]
+fn lit_signed_int_test() -> Result<()> {
+    assert_eq!(
+        parse_str::<LitSignedInt>("100")?.0,
+        parse_str::<Expr>("100")?
+    );
+
+    assert_eq!(
+        parse_str::<LitSignedInt>("-100")?.0,
+        parse_str::<Expr>("-100")?
+    );
+
+    assert!(parse_str::<LitSignedInt>("+100").is_err());
+    assert!(parse_str::<LitSignedInt>("100 + 100").is_err());
+    assert!(parse_str::<LitSignedInt>("100 < n").is_err());
+    assert!(parse_str::<LitSignedInt>("100 <= n").is_err());
+    assert!(parse_str::<LitSignedInt>("n < 100").is_err());
+    assert!(parse_str::<LitSignedInt>("n <= 100").is_err());
+
+    Ok(())
+}
+
+impl Parse for Key {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let is_le = |op: &BinOp| match op {
+            BinOp::Le(_) => true,
+            _ => false,
+        };
+
+        let is_lt_or_le = |op: &BinOp| match op {
+            BinOp::Lt(_) | BinOp::Le(_) => true,
+            _ => false,
+        };
+
+        let is_lit_signed_int = |e: &Expr| match e {
+            Expr::Lit(ExprLit { .. }) => true,
+            Expr::Unary(ExprUnary { expr, .. }) => match expr.deref() {
+                Expr::Lit(ExprLit { .. }) => true,
+                _ => false,
+            },
+            _ => false,
+        };
+
+        let lower_bound = (|| -> Option<(Expr, bool)> {
+            let ahead = input.fork();
+            let _ = ahead.parse::<LitSignedInt>().ok()?;
+            let op = ahead.parse::<BinOp>().ok()?;
+            if is_lt_or_le(&op) {
+                let b = input.parse::<LitSignedInt>().ok()?;
+                let op = input.parse::<BinOp>().ok()?;
+                Some((b.0, is_le(&op)))
+            } else {
+                None
+            }
+        })();
+
+        let (expr, upper_bound) = match input.parse()? {
+            Expr::Binary(ExprBinary {
+                left, op, right, ..
+            }) if is_lt_or_le(&op) => {
+                if is_lit_signed_int(right.deref()) {
+                    (*left, Some((*right, is_le(&op))))
+                } else {
+                    Err(Error::new(
+                        Span::call_site(),
+                        "upper_bound should be integer literal",
+                    ))?
+                }
+            }
+
+            e => (e, None),
+        };
+
+        Ok(Key {
+            expr,
+            lower_bound,
+            upper_bound,
+        })
+    }
+}
+
+#[test]
+fn parse_key_test() -> Result<()> {
+    assert_eq!(
+        parse_str::<Key>("n")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: None,
+            upper_bound: None,
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("n < 100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: None,
+            upper_bound: Some((parse_str("100")?, false)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("n <= 100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: None,
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 < n")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("-100")?, false)),
+            upper_bound: None,
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("0 <= n")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("0")?, true)),
+            upper_bound: None,
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 < n < 100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("-100")?, false)),
+            upper_bound: Some((parse_str("100")?, false)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("0 <= n < -100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("0")?, true)),
+            upper_bound: Some((parse_str("-100")?, false)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 < n <= 100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("-100")?, false)),
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 <= n <= 100")?,
+        Key {
+            expr: parse_str("n")?,
+            lower_bound: Some((parse_str("-100")?, true)),
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 <= n + 100 <= 100")?,
+        Key {
+            expr: parse_str("n + 100")?,
+            lower_bound: Some((parse_str("-100")?, true)),
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("-100 <= 100 + n <= 100")?,
+        Key {
+            expr: parse_str("100 + n")?,
+            lower_bound: Some((parse_str("-100")?, true)),
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert_eq!(
+        parse_str::<Key>("0 <= (if n >= 0 { n * 2 } else { -n * 2 + 1 }) <= 100")?,
+        Key {
+            expr: parse_str("(if n >= 0 { n * 2 } else { -n * 2 + 1 })")?,
+            lower_bound: Some((parse_str("0")?, true)),
+            upper_bound: Some((parse_str("100")?, true)),
+        }
+    );
+
+    assert!(parse_str::<Key>("n < n").is_err());
+    assert!(parse_str::<Key>("n < n < n < n").is_err());
+
+    // FIXME: make below exprs to fail
+    // assert!(parse_str::<Key>("100 < 100").is_err());
+    // assert!(parse_str::<Key>("n > n"));
+
+    Ok(())
+}
+
+struct Keys(Vec<Key>);
+
+impl Parse for Keys {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Keys(
+            input
+                .parse_terminated::<Key, Token![,]>(Key::parse)?
+                .into_iter()
+                .collect::<Vec<Key>>(),
+        ))
+    }
+}
+
+#[test]
+fn parse_keys_test() -> Result<()> {
+    assert_eq!(parse_str::<Keys>("n")?.0, vec![parse_str::<Key>("n")?]);
+
+    assert_eq!(
+        parse_str::<Keys>("n, m")?.0,
+        vec![parse_str::<Key>("n")?, parse_str::<Key>("m")?]
+    );
+
+    assert_eq!(
+        parse_str::<Keys>("n <= 100, m <= 50")?.0,
+        vec![parse_str::<Key>("n <= 100")?, parse_str::<Key>("m <= 50")?]
+    );
+
+    assert_eq!(
+        parse_str::<Keys>("50 <= n, m <= 50")?.0,
+        vec![parse_str::<Key>("50 <= n")?, parse_str::<Key>("m <= 50")?]
+    );
+
+    Ok(())
 }
 
 #[proc_macro_attribute]
 pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attr = parse_macro_input!(attr as AttributeArgs);
+    dbg!(&attr);
+
+    let keys = parse_macro_input!(attr as Keys).0;
     let item_fn = parse_macro_input!(item as ItemFn);
 
-    let args = MemoiseArgs::from_list(&attr).unwrap();
     let fn_sig = item_fn.sig;
     let fn_block = item_fn.block;
 
@@ -81,11 +393,15 @@ pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
         panic!("function return type is required");
     };
 
-    let lengths = args.keys.values().collect::<Vec<_>>();
+    // TODO: support unbound keys
+    let lengths = keys
+        .iter()
+        .map(|r| r.len().unwrap())
+        .collect::<Vec<usize>>();
 
     let cache_type = lengths.iter().rev().fold(
         parse_quote! { Option<#ret_type> },
-        |acc: Type, limit| parse_quote! { [#acc; #limit + 1] },
+        |acc: Type, limit| parse_quote! { [#acc; #limit] },
     );
 
     let cache_init = lengths
@@ -93,17 +409,24 @@ pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
         .rev()
         .fold(parse_quote! { None }, |acc: Expr, limit| {
             parse_quote! {
-                [#acc; #limit + 1]
+                [#acc; #limit]
             }
         });
 
-    let key_vars = args
-        .keys
-        .keys()
-        .map(|k| Ident::new(k, Span::call_site()))
-        .collect::<Vec<_>>();
+    let key_vars = keys
+        .iter()
+        .map(|r| {
+            let e = &r.expr;
+            let ofs = r.offset();
+            if ofs != 0 {
+                parse_quote! { ((#e) as i64 - (#ofs)) as usize }
+            } else {
+                parse_quote! { (#e) as usize }
+            }
+        })
+        .collect::<Vec<Expr>>();
 
-    let reset_expr = (0..args.keys.len()).fold(quote! { *r = None }, |acc, _| {
+    let reset_expr = (0..keys.len()).fold(quote! { *r = None }, |acc, _| {
         quote! {
             for r in r.iter_mut() {
                 #acc
@@ -118,7 +441,7 @@ pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
-    let reset_fn = Ident::new(
+    let reset_fn_name = Ident::new(
         &format!("{}_reset", fn_sig.ident.to_string()),
         Span::call_site(),
     );
@@ -128,7 +451,7 @@ pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
             static #cache_ident: std::cell::RefCell<#cache_type> =
                 std::cell::RefCell::new(#cache_init));
 
-        fn #reset_fn() {
+        fn #reset_fn_name() {
             #cache_ident.with(|cache| #reset_expr);
         }
 
@@ -141,12 +464,14 @@ pub fn memoise(attr: TokenStream, item: TokenStream) -> TokenStream {
 
             #cache_ident.with(|cache| {
                 let mut bm = cache.borrow_mut();
-                bm #([#key_vars])* = Some(ret);
+                bm#([#key_vars])* = Some(ret);
             });
 
             ret
         }
     };
+
+    eprintln!("{}", gen.to_string());
 
     gen.into()
 }
--- tests/test.rs
+++ tests/test.rs
@@ -2,8 +2,8 @@ extern crate memoise;
 
 use memoise::memoise;
 
-#[memoise(keys(n = 100))]
-fn fib(n: usize) -> usize {
+#[memoise(n <= 100)]
+fn fib(n: i64) -> i64 {
     if n == 0 || n == 1 {
         return n;
     }
@@ -11,19 +11,33 @@ fn fib(n: usize) -> usize {
 }
 
 #[test]
-fn test_fib() {
+fn fib_test() {
     assert_eq!(267914296, fib(42));
 }
 
-#[memoise(keys(n = 100, m = 50))]
-fn comb(n: usize, m: usize) -> usize {
-    if m == 0 {
+#[memoise(-100 <= n <= 100)]
+fn foo(n: i64) -> i64 {
+    if n == -100 {
+        n
+    } else {
+        foo(n - 1)
+    }
+}
+
+#[test]
+fn foo_test() {
+    assert_eq!(foo(100), -100);
+}
+
+#[memoise(n <= 100, k <= 50)]
+fn comb(n: usize, k: usize) -> usize {
+    if k == 0 {
         return 1;
     }
     if n == 0 {
         return 0;
     }
-    comb(n - 1, m - 1) + comb(n - 1, m)
+    comb(n - 1, k - 1) + comb(n - 1, k)
 }
 
 #[test]
@@ -37,4 +51,24 @@ fn comb_test() {
 #[test]
 fn reset_test() {
     comb_reset();
+}
+
+#[memoise(-100 <= n <= 100)]
+fn test1(n: i32) -> usize {
+    panic!()
+}
+
+#[memoise(n + m <= 10000)]
+fn test2(n: i32, m: i32) -> usize {
+    panic!()
+}
+
+#[memoise(n * 100 + m <= 10000)]
+fn test3(n: i32, m: i32) -> usize {
+    panic!()
+}
+
+#[memoise(-10000 <= n * 100 + m <= 10000)]
+fn test4(n: i32, m: i32) -> usize {
+    panic!()
 }
